ece24ca5ecbc320b35d6befc164e44b082432df9
Oumaima Hahati <hahatioumaima@gmail.com>
2025-05-13 10:24:37 +0100

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'orayma',
+    version= '0.0.0',
+    author= 'Bappy Ahmed',
+    author_email= 'entbappy73@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
